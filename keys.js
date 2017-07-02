8e121145021dfb8f34bf582a5e4a0a3d759cb36b
Mark Bui <tvyybui@yahoo.com>
2017-07-02 12:43:27 -0700

    starting homework

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'DOmZo9JjOihCBmkTmLpNlLqzO',
+  consumer_secret: 'ijSMk3ueh9xDSzCTWlpfuMUlxlBlfQJMlVG7PNvaAFOuO527yw',
+  access_token_key: '881169669561794562-kZIqoclpyAxTkiWKW0UxA1Po0gsu0Zi',
+  access_token_secret: 'WDGHY90usPXLq19sYsgFq3CZuEpYbZQyVUE8bf81Drt4v',
+}
---
+++ liri.js
@@ -0,0 +1,9 @@
+var keys = require('./keys.js');
+var keyList = keys.twitterKeys;
+var keyArray = [];
+keyArray.push(keyList);
+
+var fs = require('fs');
+var request = require('request');
+var
+
